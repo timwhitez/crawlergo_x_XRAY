b30ac692063bb0d0779464f3e23245fee9833b83
jhin <779789571@qq.com>
2020-11-30 21:48:18 +0800

    增加自定义请求头，避免crawelergo特征识别

--- launcher_new.py
+++ launcher_new.py
@@ -5,7 +5,17 @@ import threading
 import subprocess
 import requests
 import warnings
+import json
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+
 warnings.filterwarnings(action='ignore')
+
+def get_random_headers():
+    headers = {'User-Agent': ua.random}
+
+    return headers
 
 
 def opt2File(paths):
@@ -26,7 +36,7 @@ def opt2File2(subdomains):
 
 def main(data1):
 	target = data1
-	cmd = ["./crawlergo", "-c", "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe","-t", "5","-f","smart","--fuzz-path", "--push-to-proxy", "http://127.0.0.1:7888/", "--push-pool-max", "10","--output-mode", "json" , target]
+	cmd = ["./crawlergo/crawlergo", "-c", "/root/crawlergo_x_XRAY/crawlergo/chrome-linux/chrome","-t", "5","-f","smart","--fuzz-path","--custom-headers",json.dumps(get_random_headers()()), "--push-to-proxy", "http://127.0.0.1:7788/", "--push-pool-max", "10","--output-mode", "json" , target]
 	rsp = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	output, error = rsp.communicate()
 	try:
