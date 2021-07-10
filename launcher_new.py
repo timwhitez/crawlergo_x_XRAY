e81ab879ec43706abc6ca332be342009cad412af
thelsa <lsasguge196@gmail.com>
2021-07-10 22:04:35 +0800

    add verify_ssl=False and cache=False to solve fake-useragent problem

--- launcher_new.py
+++ launcher_new.py
@@ -8,7 +8,7 @@ import warnings
 import json
 from fake_useragent import UserAgent
 
-ua = UserAgent(use_cache_server=False)
+ua = UserAgent(use_cache_server=False,verify_ssl=False,cache=False)
 
 warnings.filterwarnings(action='ignore')
 
